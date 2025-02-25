6911ec26aab0121dbdd512e42878ab4f9bc0997d
LETRA <pedro.pardim.pc@gmail.com>
2025-02-25 08:01:20 -0300

    refactor(ApiConsumer): optimize api consumer

--- src/main/java/me/pedro/ApiConsumer.java
+++ src/main/java/me/pedro/ApiConsumer.java
@@ -1,12 +1,11 @@
 package me.pedro;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-import org.json.JSONArray; 
-import org.json.JSONObject;
 
 public class ApiConsumer {
   public static void main(String[] args) {
@@ -15,57 +14,65 @@ public class ApiConsumer {
       return;
     }
 
+    String username = args[0];
     try {
-      
-      URL url = new URL("https://api.github.com/users/" + args[0] + "/events");
-
-      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-      conn.setRequestMethod("GET");
-      conn.setRequestProperty("Accept", "application/json");
-
-      if (conn.getResponseCode() != 200) {
-        throw new RuntimeException("Erro HTTP: " + conn.getResponseCode());
-      }
-
-      BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-      StringBuilder response = new StringBuilder();
-      String line;
-      while ((line = br.readLine()) != null) {
-        response.append(line);
-      }
-      br.close();
-
-      String responseStr = response.toString();
-      JSONArray jsonEvents = new JSONArray(responseStr);
-      System.out.println("Output:");
-      for (int i = 0; i < jsonEvents.length(); i++) {
-        System.out.println("- " + displayActivity(jsonEvents.getJSONObject(i)));
-      }
-
-      conn.disconnect();
-
+      JSONArray jsonEvents = fetchGithubEvents(username);
+      displayEvents(jsonEvents);
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
 
-  public static String displayActivity(JSONObject events) {
-    switch (events.getString("type")) {
+  private static JSONArray fetchGithubEvents(String username) throws Exception {
+    URL url = new URL("https://api.github.com/users/" + username + "/events");
+    HttpURLConnection conn = (HrrpURLConnection) url.openConnection();
+    conn.setRequestMethod("GET");
+    conn.setRequestProperty("Accept", "application/json");
+
+    if (conn.getResponseCode != 200) {
+      throw new RuntimeExceoption("Erro HTTP: " + conn.getResponseCode());
+    }
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+    StringBuilder response = new StringBuilder();
+    String line;
+    while ((line = br.readLine()) != null) {
+      response.append(line);
+    }
+    br.close();
+    conn.disconnet();
+
+    return new JSONArray(response.toString());
+  }
+
+  private static void displayEvents(JSONArray jsonEvents) {
+    System.out.println("Output:");
+    for (int i = 0; i < jsonEvents.length(); i++) {
+      System.out.println("- " + displayActivity(jsonEvents.getJSONObject(i)));
+    }
+  }
+
+  private static String displayActivity(JSONObject event) {
+    String eventType = event.getString("type");
+    JSONObject payload = event.optJSONObject("payload");
+    JSONObject repo = event.getJSONObject("repo");
+
+    switch (eventType) {
       case "PushEvent": 
         return String.format("Pushed %d commit(s) to %s", 
-            events.getJSONObject("payload").getJSONArray("commits").length(), events.getJSONObject("repo").getString("name"));
+            payload.getJSONArray("commits").length(), repo.getString("name"));
       case "IssuesEvent":
         return String.format("");
       case "WatchEvent":
-        return String.format("Starred %s", events.getJSONObject("repo").getString("name"));
+        return String.format("Starred %s", repo.getString("name"));
       case "ForkEvent":
-        return String.format("Forked %s", events.getJSONObject("repo").getString("name"));
+        return String.format("Forked %s", repo.getString("name"));
       case "CreateEvent":
         return String.format("Created %s in %s", 
-            events.getJSONObject("payload").getString("ref_type"), events.getJSONObject("repo").getString("name"));
+            payload.getString("ref_type"), repo.getString("name"));
       default:
         return String.format("%s in %s", 
-            events.getString("type").replace("Event", ""), events.getJSONObject("repo").getString("name"));
+            eventType.replace("Event", ""), repo.getString("name"));
     }
   }
 }
